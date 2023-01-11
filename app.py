f96340174170a74ec6eaf85023d52836436b7991
Harishapc <122357955+Harishapc@users.noreply.github.com>
2023-01-11 19:22:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request
 import requests
-
+from twilio.twiml.messaging_response import MessagingResponse
 from pymongo import MongoClient
 from datetime import datetime
 
@@ -10,18 +10,19 @@ users = db["users"]
 orders = db["orders"]
 
 app = Flask(__name__)
-
+server = app.server
 
 @app.route('/', methods=['GET', 'POST'])
 def reply():
     text = request.form.get("Body")
     number = request.form.get("From")
-    number = number.replace("whatsapp:", "")[: -2]
-    response ={"reply": ""}
+    number = number.replace("whatsapp:", "")
+    response = MessagingResponse()
+    msg.media("https://images.unsplash.com/photo-1608365151231-7dbed3034787?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80","Welcom")
     user = users.find_one({"number": number})
 
-    if False == bool(user):
-        response["reply"]+='\n' +(
+    if bool(user) ==False :
+        response.message(
             "Hi, thanks for contacting *The Red Velvet* \n You can choose from one of th options below: "
             "\n\n*Type*\n\n1️⃣ TO *Contact* us \n2️⃣ To *order* snacks \n3️⃣To know oue *working hours* \n4️⃣ To get "
             "our*Address*")
@@ -30,36 +31,36 @@ def reply():
         try:
             option = int(text)
         except:
-            response["reply"]+='\n' +("Please enter a valid response")
+            response.message("Please enter a valid response")
             return str(response)
 
         if option == 1:
-            response["reply"]+='\n' +("you can contact us through phone or email. \n\n *phone* 997892307634 \n *E-mail* : "
+            response.message("you can contact us through phone or email. \n\n *phone* 997892307634 \n *E-mail* : "
                              "rishandrishcompanies@gmail.com")
         elif option == 2:
-            response["reply"]+='\n' +("you have entered *ordering mode*.")
+            response.message("you have entered *ordering mode*.")
             users.update_one(
                 {"number": number}, {"$set": {"status": "ordering"}})
-            response["reply"]+='\n' +(
+            response.message(
                 "You can select one of the following cakes to order, \n\n *1️⃣Red Velvet*  \n2️⃣Dark Forest "
                 "\n3️⃣Ice Cream Cake \n4️⃣Plum Cake \n5️⃣Sponge Cake \n6️⃣Genoise Cake \n7️⃣Carrot Cake "
                 "\n8️⃣Butterscotch \n0️⃣Go Back* ")
         elif option == 3:
-            response["reply"]+='\n' +("We work on everyday *9 AM to 5 PM*")
+            response.message("We work on everyday *9 AM to 5 PM*")
         elif option == 4:
-            response["reply"]+='\n' +("We work many centres across the city our main centre at *Bangalore*")
+            response.message("We work many centres across the city our main centre at *Bangalore*")
         else:
-            response["reply"]+='\n' +("Please enter a valid response")
+            response.message("Please enter a valid response")
     elif user["status"] == "ordering":
         try:
             option = int(text)
         except:
-            response["reply"]+='\n' +("Please enter a valid response")
+            response.message("Please enter a valid response")
             return str(response)
         if option == 0:
             users.update_one(
                 {"number": number}, {"$set": {"status": "main"}})
-            response["reply"]+='\n' +(" You can choose from one of th options below: "
+            response.message(" You can choose from one of th options below: "
                              "\n\n*Type*\n\n1️⃣ TO *Contact* us \n2️⃣ To *order* snacks \n3️⃣To know oue *working hours* \n4️⃣ To get "
                              "our*Address*")
         elif 1 <= option <= 9:
@@ -70,19 +71,19 @@ def reply():
             {"number": number}, {"$set": {"status": "address"}})
             users.update_one(
             {"number": number}, {"$set": {"item": selected}})
-            response["reply"]+='\n' +("Excellent choice😉")
-            response["reply"]+='\n' +("Please Enter your address to confirm the order")
+            response.message("Excellent choice😉")
+            response.message("Please Enter your address to confirm the order")
         else:
-          response["reply"]+='\n' +("Please enter a valid response")
+          response.message("Please enter a valid response")
     elif user["status"] == "address":
         selected = user["item"]
-        response["reply"]+='\n' +("Thanks for shopping with us")
-        response["reply"]+='\n' +(f" your order for {selected} has been received and will be delivered with in an hour")
+        response.message("Thanks for shopping with us")
+        response.message(f" your order for {selected} has been received and will be delivered with in an hour")
         orders.insert_one({"number": number, "item": selected, "address": text, "order_time": datetime.now()})
         users.update_one(
             {"number": number}, {"$set": {"status": "ordered"}})
     elif user["status"] == "ordered":
-        response["reply"]+='\n' +(
+        response.message(
             "Hi, thanks for contacting  again * The Red Velvet* \n You can choose from one of th options below: "
             "\n\n*Type*\n\n1️⃣ TO *Contact* us \n2️⃣ To *order* snacks \n3️⃣To know oue *working hours* \n4️⃣ To get "
             "our*Address*")
